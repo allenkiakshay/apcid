0ae18ee58fdc976bdf84949aca61c80b1bf95643
Harsh Carpenter <harshcarpenter06902@gmail.com>
2025-07-21 01:37:03 +0530

    All files are not mandatory

--- src/components/Userpage/textArea.tsx
+++ src/components/Userpage/textArea.tsx
@@ -395,6 +395,10 @@ export const TextArea: React.FC<TextAreaProps> = ({ setMessage, setSubmitStatus 
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded-2xl shadow-lg border border-gray-100">
       {/* Header */}
+
+      <div className=" py-2 bg-gray-100 rounded-2xl shadow-lg my-5">
+        <h2 className="text-2xl font-semibold text-gray-800 text-center">Section 1</h2>
+      </div>
       
        <div className="mb-4 bg-gray-50 border border-gray-200 p-4 rounded-xl shadow-inner">
         <h3 className="text-sm font-semibold text-gray-700 mb-2">Sample Text</h3>
